6f54a1e42eb088fb29ca528fa064799e5a35d6bd
Saad Bennani <saadbennani@Vamsis-MBP.localdomain>
2019-04-11 20:42:38 -0700

    number guesser

---
+++ js_labs/lab_2/guess.js
@@ -0,0 +1,64 @@
+
+const computerChoice = Math.floor(Math.random() * 11)
+let counter = 0
+let computerResponse = ''
+let guessCorrect = false
+//functions
+
+const checkNumber = (computerChoice, userGuess) => {
+  if (computerChoice < userGuess) {
+    counter ++
+    result.value = "That wasn't it, try a lower number!"
+
+  }
+  else if (computerChoice > userGuess) {
+    counter ++
+    result.value = "That wasn't it, try a higher number!"
+
+  }
+  else if (computerChoice === userGuess) {
+    counter ++
+    result.value = `Yes, that was it! It only took ${counter} tries. `
+    guessCorrect = true
+
+  }
+
+  return guessCorrect
+}
+
+
+
+// selectors
+const submitBtn = document.querySelector('#submit')
+const userGuess = document.querySelector('#userGuess')
+const result = document.querySelector('#result')
+//event listeners
+
+submitBtn.addEventListener('click', (evt) =>{
+  const x = parseInt(userGuess.value)
+  console.log(x)
+  guessCorrect = checkNumber(computerChoice, x)
+  console.log(guessCorrect)
+  guessCorrect.value = `${computerResponse}`
+  if (computerChoice === userGuess) {
+    userGuess = userGuess.value
+  } else if (computerChoice != userGuess) {
+    userGuess.value = ''
+  }
+
+})
+
+userGuess.addEventListener('keydown', (evt) => {
+  if (evt.key==='Enter') {
+  const x = parseInt(userGuess.value)
+  console.log(x)
+  guessCorrect = checkNumber(computerChoice, x)
+  console.log(guessCorrect)
+  guessCorrect.value = `${computerResponse}`
+  if (computerChoice === userGuess) {
+    userGuess.value = userGuess.value
+  } else if (computerChoice != userGuess) {
+    userGuess.value = ''
+  }
+}
+})
