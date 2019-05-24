5c5bcb3dc89a01462d4a35df7a388c529ce5f38d
SaadyBennani <sbennani23@gmail.com>
2019-05-24 16:24:05 -0700

    v1

---
+++ js_labs/lab_5/main.js
@@ -0,0 +1,24 @@
+const websiteList = [
+    'https://www.reddit.com',
+    'https://www.vice.com',
+    'https://www.moroccoworldnews.com',
+    'https://www.nhl.com',
+    'https://www.nba.com',
+    'https://www.mlb.com',
+    'https://www.cia.gov/library/publications/the-world-factbook/',
+    'https://www.nfl.com',
+    ]
+
+const randomUrl = (list) =>{
+  let i = Math.floor(Math.random() * list.length);
+  return list[i]
+}
+
+// DOM selector
+const rButton = document.querySelector('#redirect')
+
+//event listener
+rButton.addEventListener('click', (evt)=> {
+  console.log('Bloooop')
+  window.location.href = randomUrl(websiteList);
+})
